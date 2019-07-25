b435460a86b2cfc7f8a075c0e5c6169d3bf3459f
Ponas <mykolas.peteraitis@gmail.com>
2019-07-26 00:04:55 +0300

    realtime

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,174 @@
+#![feature(atomic_min_max)]
+
 extern crate image;
 extern crate time;
 extern crate rayon;
 extern crate num;
+extern crate minifb;
 
-use std::env;
 use rayon::prelude::*;
-use std::sync::atomic::{AtomicU64, Ordering};
-use std::io;
-use std::io::prelude::*;
+use std::sync::atomic::{AtomicU32, Ordering};
+
+const ZOOM_POWER: f64 = 3.0;
+
+struct Zoom {
+	center: (f64, f64),
+	zoom: f64
+}
 
 fn main() {
 
-	// Get the args
-	let args: Vec<String> = env::args().collect();
+	let mut width = 1024;
 
-	// Get the image
-	if args.len() != 2 {
-		println!("Usage: ./mandelbrot <width>");
+	let args: Vec<String> = std::env::args().collect();
+
+	if args.len() == 2 {
+		width = args[1].parse::<usize>().expect("Could not parse the window width parameter.")
+	}
+	if args.len() > 2 {
+		println!("Too many arguments!");
 		return;
 	}
+	let height = (width as f64*2.4/3.5) as usize;
 
-	let width = args[1].parse::<u32>().unwrap();
-	let height = (width as f32*9.0/16.0) as u32;
-	let iterations_per_pixel = (width as f64/25f64) as u32;
+	let mut window = match minifb::Window::new("Mandelbrot", width, height, minifb::WindowOptions::default()) {
+		Ok(win) => win,
+		Err(err) => {
+			println!("Unable to create window {}", err);
+			return;
+		}
+	};
+	window.set_cursor_style(minifb::CursorStyle::Crosshair);
 
-	println!("This size will take {:.6} GB of RAM, are you sure you want to do this?", (width as u64*height as u64*3u64) as f64/1000f64/1000f64/1000f64);
-	let _ = io::stdin().read(&mut [0u8]).unwrap();
+	let mut zoom = Zoom{center: (-0.25, 0.), zoom: 1.0};
 
-	let start = time::precise_time_s();
+	let mut image_buffer: Vec<u32> = vec![0u32; width*height];
+
+	generate_image(width, height, &zoom, &mut image_buffer);
+
+	let mut left_clicked = false;
+	let mut right_clicked = false;
+
+	while window.is_open() {
+		window.update_with_buffer(&image_buffer[..]);
+
+		// Left click - zoom
+		let left_pressed = window.get_mouse_down(minifb::MouseButton::Left);
+		if left_pressed && !left_clicked {
+			left_clicked = true;
+
+			let click_pos = window.get_mouse_pos(minifb::MouseMode::Clamp);
+			match click_pos {
+				Some(pos) => {
+					// Zoom
+
+					let old_zoom = Zoom{center: zoom.center, zoom: zoom.zoom};
+
+					zoom.zoom *= ZOOM_POWER;
+					zoom.center = (
+						(old_zoom.center.0 - 2.0/old_zoom.zoom) + (pos.0 as f64/(width-1) as f64) * 3.5/old_zoom.zoom,
+						(old_zoom.center.1 - 1.2/old_zoom.zoom) + (pos.1 as f64/(height-1) as f64) * 2.4/old_zoom.zoom
+					);
+					generate_image(width, height, &zoom, &mut image_buffer);
+				},
+				None => ()
+			}
+		}
+		if !left_pressed && left_clicked { left_clicked = false;}
+
+		// Right click - unzoom
+		let right_pressed = window.get_mouse_down(minifb::MouseButton::Right);
+		if right_pressed && !right_clicked {
+			right_clicked = true;
+
+			let click_pos = window.get_mouse_pos(minifb::MouseMode::Clamp);
+			match click_pos {
+				Some(pos) => {
+					// Unzoom
+
+					let old_zoom = Zoom{center: zoom.center, zoom: zoom.zoom};
+
+					zoom.zoom /= ZOOM_POWER;
+					zoom.center = (
+						(old_zoom.center.0 - 2.0/old_zoom.zoom) + (((width-1) as f64-pos.0 as f64)/(width-1) as f64) * 3.5/old_zoom.zoom,
+						(old_zoom.center.1 - 1.2/old_zoom.zoom) + (((height-1) as f64-pos.1 as f64)/(height-1) as f64) * 2.4/old_zoom.zoom
+					);
+
+					generate_image(width, height, &zoom, &mut image_buffer);
+				},
+				None => ()
+			}
+		}
+		if !right_pressed && right_clicked { right_clicked = false;}
+
+	}
+
+
+
+}
+
+fn generate_image(width: usize, height: usize, zoom: &Zoom, buffer: &mut Vec<u32>) {
+
+	let iterations_per_pixel = width as u32;
+
+	let mut escape = vec![iterations_per_pixel; width*height];
+
+	let lowest = AtomicU32::new(iterations_per_pixel as u32);
+	let highest = AtomicU32::new(0u32);
 	
-	let img = image::ImageBuffer::<image::Rgb<u8>, Vec<u8>>::new(width, height);
-
-	// Distribute the work amongst the cores
-	let mut buffer = img.clone().into_vec();
-
-	// Status bar stuff
-	let rows_done: AtomicU64 = AtomicU64::new(0_u64);
-
-	// Split the image into rows
-    buffer.par_chunks_mut(width as usize * 3usize)
+	// Get the escapes
+    escape.par_chunks_mut(width as usize)
         .enumerate()
         .for_each(|(y, row)| {
             // Iterate through all pixels in this row
             for x in 0..width {
 				// Check if it's in the mandelbrot set
-				match is_point_in_set(x, y, iterations_per_pixel, width, height) {
+				let real: f64 = (zoom.center.0 - 2.0/zoom.zoom) + (x as f64/(width-1) as f64) * 3.5/zoom.zoom;
+				let imaginary: f64 = (zoom.center.1 - 1.2/zoom.zoom) + (y as f64/(height-1) as f64) * 2.4/zoom.zoom;
+
+				match is_point_in_set(real, imaginary, iterations_per_pixel) {
 					Some(itr) => {
-						let mut r_val = (itr as f32/iterations_per_pixel as f32)*255f32*2f32;
-						if r_val > 255f32 {
-							r_val = 255f32;
-						}
-						let mut gb_val = (1f32-(itr as f32/iterations_per_pixel as f32)) * -255f32 + 255f32;
-						if gb_val < 0f32 {
-							gb_val = 0f32;
-						}
-						row[x as usize * 3usize]		 = r_val as u8;
-						row[x as usize * 3usize +1usize] = gb_val as u8;
-						row[x as usize * 3usize +2usize] = gb_val as u8;
+						row[x as usize] = itr;
+						lowest.fetch_min(itr as u32, Ordering::Relaxed);
+						highest.fetch_max(itr as u32, Ordering::Relaxed);
 					},
 					None => {
-						row[x as usize * 3usize]		 = 255u8;
-						row[x as usize * 3usize +1usize] = 255u8;
-						row[x as usize * 3usize +2usize] = 255u8;
+						row[x as usize] = iterations_per_pixel as u32;
+						highest.fetch_max(iterations_per_pixel as u32, Ordering::Relaxed);
 					}
 				}
 	        }
-	        let rows_done_new = rows_done.fetch_add(1_u64, Ordering::Relaxed) + 1u64;
-	        // Update the status bar
-			let message = format!("{:>12} of {:>12} iterations done ({:>5.2}%). ETA: {:>05.1} seconds.\r", 
-				rows_done_new*width as u64*width as u64/25u64,
-				width as u64*height as u64*width as u64/25u64,
-				((rows_done_new*width as u64*width as u64) as f64 / (width as u64*height as u64*width as u64) as f64)*100f64,
-				((width as u64*height as u64*width as u64) as f64 - (rows_done_new*width as u64*width as u64) as f64) / ((rows_done_new*width as u64*width as u64) as f64 / (time::precise_time_s()-start))
-			);
-			print!("{:<}", message);
         });
 
-    let final_image: image::RgbImage = image::ImageBuffer::from_vec(width, height, buffer).unwrap();
+    let h = highest.load(Ordering::Relaxed);
+    let l = lowest.load(Ordering::Relaxed);
 
-	// Save
-	println!("\n\nGeneration finished. It took {} seconds", time::precise_time_s()-start);
-	println!("Saving...");
-	final_image.save("mandelbrot.png").unwrap();
-	println!("{}x{} mandelbrot set image generated! It took {} seconds.", width, height, time::precise_time_s()-start);
+    // Now add colors depending on the highest, lowest escapes
+    buffer.par_chunks_mut(width as usize)
+	    .enumerate()
+	    .for_each(|(y, row)| {
+	        // Iterate through all pixels in this row
+	        for x in 0..width {
+	        	let red = 255f64 *
+	        		(escape[(y*width+x) as usize] - l) as f64 /
+	        		(h - l) as f64;
+				let mut greenblue = 2.0*255f64 *
+					(escape[(y*width+x) as usize] - l) as f64 /
+					(h - l) as f64;
+				if greenblue > 255f64 {
+					greenblue = 255f64;
+				}
+
+				row[x as usize] = (255u32 << 24) |
+					((red as u32) << 16) |
+					((greenblue as u32) << 8) |
+					greenblue as u32;
+	        }
+    	});
 }
 
-fn is_point_in_set(x: u32, y: usize, iterations: u32, width: u32, height: u32) -> Option<u32> {
-	let c = num::complex::Complex::new((x as f64/(width-1) as f64)*3.5f64-2.5f64, (y as f64/(height-1) as f64)*1.96875f64-0.984375f64);
+fn is_point_in_set(x: f64, y: f64, iterations: u32) -> Option<u32> {
+	let c = num::complex::Complex::new(x, y);
 	
 	let mut z = num::complex::Complex::new(0f64, 0f64);
 
