42c1d28cee96a4f76ea62e3c5197b61e636bb461
Ponas <mykolas.peteraitis@gmail.com>
2019-07-26 21:58:55 +0300

    high-res saving

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,15 @@ fn main() {
 	if args.len() == 2 {
 		width = args[1].parse::<usize>().expect("Could not parse the window width parameter.")
 	}
-	if args.len() > 2 {
+	let mut height = (width as f64*2.4/3.5) as usize;
+	if args.len() == 3 {
+		height = args[2].parse::<usize>().expect("Could not parse the window height parameter.")
+	}
+	if args.len() > 3 {
 		println!("Too many arguments!");
 		return;
 	}
-	let height = (width as f64*2.4/3.5) as usize;
+	
 
 	let mut window = match minifb::Window::new("Mandelbrot", width, height, minifb::WindowOptions::default()) {
 		Ok(win) => win,
@@ -44,13 +48,36 @@ fn main() {
 
 	let mut image_buffer: Vec<u32> = vec![0u32; width*height];
 
-	generate_image(width, height, &zoom, &mut image_buffer);
+	generate_image(width, height, &zoom, &mut image_buffer, 1.0);
 
 	let mut left_clicked = false;
 	let mut right_clicked = false;
 
 	while window.is_open() {
-		window.update_with_buffer(&image_buffer[..]);
+		// S letter - high-res save
+		if window.is_key_pressed(minifb::Key::S, minifb::KeyRepeat::No) {
+			println!("Saving...");
+			let new_width = width*3;
+			let new_height = height*3;
+			generate_image(new_width, new_height, &zoom, &mut image_buffer, 3.0);
+
+			let mut buffer: Vec<u8> = vec![0; new_width*3*new_height];
+			buffer.par_chunks_mut(new_width as usize * 3usize)
+			    .enumerate()
+			    .for_each(|(y, row)| {
+			        // Iterate through all pixels in this row
+			        for x in 0..new_width {
+			        	let bit_pixel: u32 = image_buffer[(y*new_width+x) as usize];
+			        	row[(x*3) as usize] 	= ((bit_pixel >> 16) & 0xFF) as u8;
+			        	row[(x*3 + 1) as usize] = ((bit_pixel >> 8) & 0xFF) as u8;
+			        	row[(x*3 + 2) as usize] = (bit_pixel & 0xFF) as u8;
+			        }
+		    	});
+		    let final_image: image::RgbImage = image::ImageBuffer::from_vec(new_width as u32, new_height as u32, buffer).expect("Buffer not big enough!");
+		    final_image.save(format!("highres_{}:{}:{}.png", time::now().tm_hour, time::now().tm_min, time::now().tm_sec)).unwrap();
+		    println!("Saved! {}x{}", new_width, new_height);
+		    generate_image(width, height, &zoom, &mut image_buffer, 1.0);
+		}
 
 		// Left click - zoom
 		let left_pressed = window.get_mouse_down(minifb::MouseButton::Left);
@@ -61,7 +88,7 @@ fn main() {
 			match click_pos {
 				Some(pos) => {
 					// Zoom
-
+					println!("{:?}", zoom.center);
 					let old_zoom = Zoom{center: zoom.center, zoom: zoom.zoom};
 
 					zoom.zoom *= ZOOM_POWER;
@@ -69,7 +96,7 @@ fn main() {
 						(old_zoom.center.0 - 2.0/old_zoom.zoom) + (pos.0 as f64/(width-1) as f64) * 3.5/old_zoom.zoom,
 						(old_zoom.center.1 - 1.2/old_zoom.zoom) + (pos.1 as f64/(height-1) as f64) * 2.4/old_zoom.zoom
 					);
-					generate_image(width, height, &zoom, &mut image_buffer);
+					generate_image(width, height, &zoom, &mut image_buffer, 1.0);
 				},
 				None => ()
 			}
@@ -94,22 +121,23 @@ fn main() {
 						(old_zoom.center.1 - 1.2/old_zoom.zoom) + (((height-1) as f64-pos.1 as f64)/(height-1) as f64) * 2.4/old_zoom.zoom
 					);
 
-					generate_image(width, height, &zoom, &mut image_buffer);
+					generate_image(width, height, &zoom, &mut image_buffer, 1.0);
 				},
 				None => ()
 			}
 		}
 		if !right_pressed && right_clicked { right_clicked = false;}
 
+		window.update_with_buffer(&image_buffer[..]);
 	}
 
 
 
 }
 
-fn generate_image(width: usize, height: usize, zoom: &Zoom, buffer: &mut Vec<u32>) {
+fn generate_image(width: usize, height: usize, zoom: &Zoom, buffer: &mut Vec<u32>, dimensions_scale: f64) {
 
-	let iterations_per_pixel = width as u32;
+	let iterations_per_pixel = (width as f64/ dimensions_scale) as u32;
 
 	let mut escape = vec![iterations_per_pixel; width*height];
 
@@ -144,6 +172,7 @@ fn generate_image(width: usize, height: usize, zoom: &Zoom, buffer: &mut Vec<u32
     let l = lowest.load(Ordering::Relaxed);
 
     // Now add colors depending on the highest, lowest escapes
+    *buffer = vec![0u32; width*height];
     buffer.par_chunks_mut(width as usize)
 	    .enumerate()
 	    .for_each(|(y, row)| {
